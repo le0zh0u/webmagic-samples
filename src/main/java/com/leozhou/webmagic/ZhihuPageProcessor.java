033c34d73eb29967ca0d709f2cb851ef85e41b0c
leozhou <492753662@qq.com>
2016-12-18 20:15:13 +0800

    add zhihu page processor

---
+++ src/main/java/com/leozhou/webmagic/ZhihuPageProcessor.java
@@ -0,0 +1,56 @@
+package com.leozhou.webmagic;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Html;
+
+import java.util.List;
+
+/**
+ * Created by zhouchunjie on 2016/12/18.
+ */
+public class ZhihuPageProcessor implements PageProcessor {
+
+    private Site site = Site.me().setCycleRetryTimes(5).setRetryTimes(5).setSleepTime(500).setTimeOut(3*60*1000)
+            .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36")
+            .addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+            .addHeader("Accept-Language","zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4")
+            .setCharset("UTF-8");
+
+    private static final int voteNum = 1000;
+
+
+    public void process(Page page) {
+        List<String> relativeUrl = page.getHtml().xpath("//li[@class='item clearfix']/div/a/@href").all();
+        page.addTargetRequests(relativeUrl);
+        relativeUrl= page.getHtml().xpath("//div[@id='zh-question-related-questions']//a[@class='question_link']/@href").all();
+        page.addTargetRequests(relativeUrl);
+        List<String> answers = page.getHtml().xpath("//div[@id='zh-question-answer-wrap']/div").all();
+        boolean exist = false;
+        for (String answer : answers){
+            String vote = new Html(answer).xpath("//div[@class='zm-votebar']//span[@class='count']/text()").toString();
+            if (Integer.valueOf(vote) >= voteNum){
+                page.putField("vote", vote);
+                page.putField("content", new Html(answer).xpath("//div[@class='zm-editable-content']"));
+                page.putField("userid", new Html(answer).xpath("//a[@class='author-link']/@href"));
+                exist = true;
+            }
+        }
+        if (!exist){
+            page.setSkip(true);
+        }
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args){
+        Spider.create(new ZhihuPageProcessor()).addUrl("http://www.zhihu.com/search?type=question&q=java")
+                .addPipeline(new FilePipeline("/Users/zhouchunjie/workspace/java-projects/webmagic/webmagic-samples/src/main/resources/export-sample"))
+        .thread(5).run();
+    }
+}
